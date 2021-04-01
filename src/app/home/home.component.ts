d15ce4523669c6f92a321a24a856f5b288b7329e
Dandapani <dandapani.paneerselvam@ameexusa.com>
2021-04-01 11:09:52 +0530

    Header and Footer component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { from } from 'rxjs';
-import {Router} from '@angular/router'
-import { DataServiceService } from '../data-service.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mbs-home',
@@ -9,32 +6,10 @@ import { DataServiceService } from '../data-service.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  link:string
-  register:string
 
-  @Output() newItemEvent= new EventEmitter<string>();
-  constructor(private router:Router, private dataServiceService:DataServiceService) {
-    this.link='/booklist';
-    //this.register='/registration-form'
-   }
-   goTo(data?){
-     if(data){
-       this.router.navigateByUrl(data)
-     }
-     else{
-       alert("no data")
-     }
-   }
-   update(){
-     this.dataServiceService.myFname="Mmohamed"
-     console.log("updated",this.dataServiceService.myFname,this.dataServiceService.myLname)
-      // this.newItemEvent.emit("hi");
-   }
+  constructor() { }
 
   ngOnInit(): void {
-    this.dataServiceService.myFname="Mohamed"
-    this.dataServiceService.myLname="Foise"
-    console.log("Home>>",this.dataServiceService.myFname,this.dataServiceService.myLname);
   }
 
 }
